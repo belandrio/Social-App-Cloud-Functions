252b09743cfd5805cd5effedff8e971e4d993ad2
bsinoimeri <b.sinoimeri@hotmail.com>
2020-08-03 16:29:39 +0200

    Functions with Express

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    posts: [
+        {
+            userHandle: "user",
+            text: "This is a post text",
+            createdAt: "2020-08-03T12:28:31.446Z", 
+            likeCount: 5, 
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,25 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require('express')();
 
 admin.initializeApp();
 
 
-
-exports.getPosts = functions.https.onRequest((req, res) => {
+app.get("/posts", (req, res) => {
   admin
     .firestore()
     .collection("posts")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let posts = [];
       data.forEach((doc) => {
-        posts.push(doc.data());
+        posts.push({
+          postId: doc.id,
+          text: doc.data().text,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(posts);
     })
@@ -25,14 +31,11 @@ exports.getPosts = functions.https.onRequest((req, res) => {
 
 
 
-exports.createPosts = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(400).json({ error: "Method not allowed" });
-  }
+app.post("/createPost", (req, res) => {
   const newPost = {
     text: req.body.text,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
   };
   admin
     .firestore()
@@ -45,3 +48,8 @@ exports.createPosts = functions.https.onRequest((req, res) => {
       res.status(500).json({ error: err.code });
     });
 });
+
+
+//https://baseurl.com/api/posts or https://baseurl.com/api/createPost
+
+exports.api = functions.region("europe-west3").https.onRequest(app);

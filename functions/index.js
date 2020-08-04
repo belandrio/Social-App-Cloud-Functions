9b46575a4b8d295b53abaef166af362b90e256ba
bsinoimeri <b.sinoimeri@hotmail.com>
2020-08-04 21:58:44 +0200

    User Sign Up

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,26 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const app = require('express')();
+const app = require("express")();
 
 admin.initializeApp();
 
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get("/posts", (req, res) => {
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -18,7 +30,7 @@ app.get("/posts", (req, res) => {
           postId: doc.id,
           text: doc.data().text,
           userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt
+          createdAt: doc.data().createdAt,
         });
       });
       return res.json(posts);
@@ -29,17 +41,13 @@ app.get("/posts", (req, res) => {
     });
 });
 
-
-
 app.post("/createPost", (req, res) => {
   const newPost = {
     text: req.body.text,
     userHandle: req.body.userHandle,
     createdAt: new Date().toISOString(),
   };
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .add(newPost)
     .then((doc) => {
       return res.json({ message: `document ${doc.id} created succesfully` });
@@ -49,6 +57,53 @@ app.post("/createPost", (req, res) => {
     });
 });
 
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userHandle: req.body.userHandle,
+  };
+
+  // TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.userHandle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "This handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        userHandle: newUser.userHandle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.userHandle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already is use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
 
 //https://baseurl.com/api/posts or https://baseurl.com/api/createPost
 
